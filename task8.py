ce2c6b4a1f8227b44ac021f511e29b9475100c74
Hander22101998 <hander22101998@gmail.com>
2020-03-09 17:02:13 +0600

    eigths commit

---
+++ task8.py
@@ -0,0 +1,24 @@
+def nums():
+    numbers = [1 ,- 4 , -9 , -8 , 3, 4, 5, 6 , 0]
+    polojitelnie = []
+    otricatelnie = []
+    zero = []
+    num = 0
+
+    for i in numbers :
+        if i > 0 :
+            polojitelnie.replace(1)
+        elif i < 0:
+            otricatelnie.replace(-1) 
+        else :
+            zero.replace(0)      
+    return polojitelnie , otricatelnie , zero
+print(nums())
+
+
+    
+
+print (nums())
+
+        
+
